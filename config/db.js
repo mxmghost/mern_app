57198bf78181370a266483a1c980638a3a2b4ba1
mxmghost <mxmghost@gmail.com>
2020-03-31 01:40:09 -0600

    Connected to MongoDB Atlas with mongoose

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+// Creating an async await function to resolve the promise of
+// mongoose.connect NOTE: We also set an object with some parameters
+// that allows us to supress some warnings
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
